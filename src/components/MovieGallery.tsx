a463fe264eafa839df3795aecd5db49502d6790e
Michiko Go <michikogo98@gmail.com>
2026-05-30 18:52:40 -0400

    fix: increase grid gap to accommodate hover scale effect

--- src/components/MovieGallery.tsx
+++ src/components/MovieGallery.tsx
@@ -33,7 +33,7 @@ const MovieGallery = ({ movies, loading, error, layout }: Props) => {
   const containerClass =
     layout === 'list'
       ? 'flex flex-col gap-5'
-      : 'grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'
+      : 'grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'
 
   return (
     <div className={containerClass}>
